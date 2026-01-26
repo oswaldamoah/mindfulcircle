4b21a9da3d6fd320ee3f44e413124bd678008586
Oswald <103337835+oswaldamoah@users.noreply.github.com>
2026-01-26 05:36:24 +0000

    Change image source to local path
    Updated image source path for Mindful Circle logo.

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
             <div className="flex items-center gap-3">
               <div className="flex h-20 w-20 items-center justify-center scale-[1.18]">
                 <img
-                  src="https://mindfulcircle.vercel.app/src/public/ms.png" <!---Change from vercel when hosting changes--->
+                  src="src/public/ms.png" 
                   alt="Mindful Circle logo"
                   className="h-full w-full object-contain"
                 />
